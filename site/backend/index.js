a655f3c840663387dc82fc82fdb514976d8626af
Gustavo Porto <portothree@gmail.com>
2019-07-02 13:43:24 +0100

    set handlebars as default view engine

--- site/backend/index.js
+++ site/backend/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
-const app = express()
+const app = express();
+const hbs = handlebars.create();
+
+app.engine('hbs', hbs.engine);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.send('Wazzapp')
+    res.render('main', {
+        title: 'Hey yo',
+        message: 'Example of Handlebars usage'
+    })
 });
 
 app.listen(3333);
