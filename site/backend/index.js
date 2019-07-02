c4a8b503c938628409eba862c291c4b740ff1cc9
Gustavo Porto <portothree@gmail.com>
2019-07-02 15:16:47 +0100

    config handlebars layouts

--- site/backend/index.js
+++ site/backend/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
-const hbs = handlebars.create();
 
-app.engine('hbs', hbs.engine);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', handlebars({
+  extname: '.hbs',
+  defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-    res.render('main', {
-        title: 'Hey yo',
-        message: 'Example of Handlebars usage'
-    })
+  res.render('home', {
+    title: 'Hey yo',
+    message: 'Example of Handlebars usage'
+  });
 });
 
-app.listen(3333);
+app.listen(3000);
