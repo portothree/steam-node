ef3b8c2e3a96ab103a7b33cd23721a0d9f1e4e85
Gustavo Porto <portothree@gmail.com>
2019-07-02 01:22:05 +0100

    handling trade offers

---
+++ handle-offers.js
@@ -0,0 +1,12 @@
+const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp');
+const SteamCommunity = require('steamcommunity');
+const TradeOfferManager = require('steam-tradeoffer-manager');
+
+const client = new SteamUser();
+const community = new SteamCommunity();
+const manager = new TradeOfferManager({
+    steam: client,
+    community: community,
+    language: 'en'
+});
