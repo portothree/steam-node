c6517406a9de4be3adff0c245f76ee2b4defe442
Gustavo Porto <portothree@gmail.com>
2019-07-02 01:41:38 +0100

    accept all income trades from an account we trust

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ client.logOn(logOnOptions);
 client.on('loggedOn', () => {
     console.log('Logged into Steam');
 });
+
+manager.on('newOffer', offer => {
+    if (offer.partner.getSteamID64() === '') { //trusted account id
+        offer.accept((err, status) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`Accepted offer. Status: ${status}.`);
+            }
+        });
+    }
+});
