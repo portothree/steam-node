dd344145e71110341418e060843123d78b3b9aec
Gustavo Porto <portothree@gmail.com>
2019-07-02 13:24:53 +0100

    accept all offers that we do not send any items back in the trade

---
+++ donation.js
@@ -0,0 +1,43 @@
+const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp');
+const SteamCommunity = require('steamcommunity');
+const TradeOfferManager = require('steam-tradeoffer-manager');
+
+const client = new SteamUser();
+const community = new SteamCommunity();
+const manager = new TradeOfferManager({
+  steam: client,
+  community: community,
+  language: 'en'
+});
+
+const logOnOptions = {
+    accountName: '',
+    password: '',
+    twoFactorCode: SteamTotp.generateAuthCode('') // steam shared secret
+};
+
+client.logOn(logOnOptions);
+
+client.on('loggedOn', () => {
+    console.log('Logged into Steam');
+});
+
+client.on('webSession', (sessionid, cookies) => {
+    manager.setCookies(cookies);
+  
+    community.setCookies(cookies);
+    community.startConfirmationChecker(15000, '');
+});
+
+manager.on('newOffer', offer => {
+    if (offer.itemToGive.length === 0) {
+        offer.accept((err, status) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`Donation accepted, status: ${status}`)
+            }
+        });
+    }
+})
